1ca56e8d74bdc165b78e7e384b015e136f9d693f
Simon Ndiritu <deshtey@gmail.com>
2022-01-01 08:39:05 +0300

    changed footer

--- components/Footer.js
+++ components/Footer.js
@@ -32,16 +32,7 @@ function Footer() {
                 <p>Investors</p>
                 <p>Airbnb Plus</p>
                 <p>Airbnb Luxe</p>
-            </div>
-            <div className="space-y-4 text-xs text-gray-800">
-                <h5 className="font-bold">ABOUT</h5>
-                <p>How Airbnb works</p>
-                <p>Newsroom</p>
-                <p>Investors</p>
-                <p>Airbnb Plus</p>
-                <p>Airbnb Luxe</p>
-            </div>
- 
+            </div> 
         </div>
     )
 }
